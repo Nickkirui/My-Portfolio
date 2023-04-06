cc3a82306cd3c87f0eab930af5f4d25a56bc5e46
Nick Kirui <nickkirui69@gmail.com>
2023-04-06 21:34:20 +0300

    Git

--- src/data/data.js
+++ src/data/data.js
@@ -1,34 +1,36 @@
-import WorkIm from '../Assets/Projects/workImg.png'
-import RealEst from '../Assets/Projects/realestate.png'
+import Riziki from '../Assets/Projects/Riziki.png'
+import yummy from '../Assets/Projects/yummy.png'
+import Budget from '../Assets/Projects/Budget.png'
+import cyclist from '../Assets/Projects/cyclist.png'
 
 export const data=[
     {
         id:1,
         name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        image:Riziki,
+        github:"https://github.com/Nickkirui/riziki",
+        
     },
     {
         id:2,
         name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        image:yummy,
+        github:"https://github.com/Nickkirui/yummyInn",
+        
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"JavaScript Application",
+        image:Budget,
+        github:"https://github.com/Nickkirui/budget_app",
+        
     },
     {
         id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"R data Analysis",
+        image:cyclist,
+        github:"https://github.com/Nickkirui/google-capstone",
+        
     },
 
 
